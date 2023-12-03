09ab3112612ce50d452810b646b0dbc9dd99d203
Swastik Bhattacharyya <swastikunity@gmail.com>
2023-12-03 13:20:16 +0530

    Update `model.pickle` and add web application user interface
    This commit updates the `model.pickle` file with a model saved from the latest version of `scikit-learn` and also updates the `main.py` file by adding the user interface for the Streamlit web application.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import pickle
+
+
+model = pickle.load(open('model.pickle', 'rb'))
+params = pickle.load(open('params.pickle', 'rb'))
 
 
 st.set_page_config(page_title='Bengaluru House Price Prediction', page_icon=':house:')
 st.title('Bengaluru House Price Prediction')
 st.subheader('Predict the price of a house in Bengaluru using Machine Learning')
+
+st.text_input('Total Square Feet', placeholder='Enter the total square feet area of the house')
+st.text_input('Number of Bathrooms', placeholder='Enter the number of bathrooms in the house')
+st.text_input('Number of Bedrooms', placeholder='Enter the number of bedrooms in the house')
+st.selectbox('Choose the location of the house', params['columns'])
+
+st.button('Predict', type='primary')
